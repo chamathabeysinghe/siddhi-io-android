da19afa5dfd7af4628af5b2e622a9ed55d4e8f08
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-10-31 09:55:23 +0530

    Changes for sample app

--- sample-siddhi-app/src/main/java/org/wso2/siddhi/android/sample/CustomAppActivity.java
+++ sample-siddhi-app/src/main/java/org/wso2/siddhi/android/sample/CustomAppActivity.java
@@ -1,29 +1,251 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.siddhi.android.sample;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.daimajia.swipe.adapters.BaseSwipeAdapter;
+import org.wso2.siddhi.android.sample.database.AppDbContract;
+import org.wso2.siddhi.android.sample.database.SiddhiAppDBHelper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomAppActivity extends AppCompatActivity {
+    private static final int READ_REQUEST_CODE = 42;
+    private SiddhiAppDBHelper siddhiAppDBHelper = null;
+    private View mainView;
+    private List<String> appList;
+    private ListView listView;
+    private GridViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_app);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mainView = findViewById(R.id.main_view);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                performFileSearch();
+
             }
         });
+        siddhiAppDBHelper = new SiddhiAppDBHelper(this);
+
+        this.appList = readAppFromDatabase();
+
+        this.listView = findViewById(R.id.listView);
+        this.adapter = new GridViewAdapter(this, appList);
+        this.listView.setAdapter(this.adapter);
+
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                String inputStr = readTextFromUri(uri);
+                for (String app : inputStr.split("###")) {
+                    writeAppToDatabase(app);
+                }
+                Snackbar.make(mainView, "Apps were saved", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }
+    }
+
+    private String readTextFromUri(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    inputStream));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            return stringBuilder.toString();
+        } catch (IOException e) {
+            Snackbar.make(null, "Could not read file", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        return null;
+    }
+
+    private void writeAppToDatabase(String appDefiniton) {
+        SQLiteDatabase db = siddhiAppDBHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(AppDbContract.SiddhiAppEntry.COLUMN_NAME_APP_DEFINITION, appDefiniton);
+        db.insert(AppDbContract.SiddhiAppEntry.TABLE_NAME, null, contentValues);
+    }
+
+    private List<String> readAppFromDatabase() {
+        SQLiteDatabase db = siddhiAppDBHelper.getReadableDatabase();
+        String[] projection = {AppDbContract.SiddhiAppEntry.COLUMN_NAME_APP_DEFINITION};
+
+        Cursor cursor = db.query(AppDbContract.SiddhiAppEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        List<String> items = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            String appDefinition = cursor.getString(cursor.getColumnIndex(
+                    AppDbContract.SiddhiAppEntry.COLUMN_NAME_APP_DEFINITION));
+            items.add(appDefinition);
+        }
+        return items;
+    }
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    public void performFileSearch() {
+
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+    private class GridViewAdapter extends BaseSwipeAdapter {
+
+        private Context mContext;
+        private List<String> appList;
+        private List<Boolean> isRunning;
+
+        public GridViewAdapter(Context mContext, List<String> appList) {
+            this.mContext = mContext;
+            this.appList = appList;
+            this.isRunning = new ArrayList<>();
+            for (int x = 0; x < appList.size(); x++) {
+                this.isRunning.add(false);
+            }
+        }
+
+        @Override
+        public int getSwipeLayoutResourceId(int position) {
+            return R.id.sample2;
+        }
+
+        @Override
+        public View generateView(int position, ViewGroup parent) {
+            return LayoutInflater.from(mContext).inflate(R.layout.list_view_item, null);
+        }
+
+        @Override
+        public void fillValues(int position, View convertView) {
+            TextView t = convertView.findViewById(R.id.textView);
+            t.setText(appList.get(position));
+            TextView t2 = convertView.findViewById(R.id.textView2);
+            t2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isRunning.get(position)) {
+                        if (stopApp(position)) {
+                            t2.setText("Start App");
+                            isRunning.set(position, false);
+                        } else {
+                            Toast.makeText(CustomAppActivity.this, "Can't stop the app", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        if (startApp(position)) {
+                            t2.setText("Stop App");
+                            isRunning.set(position, true);
+                        } else {
+                            Toast.makeText(CustomAppActivity.this, "Can't execute the app", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+        }
+
+        private boolean startApp(int position) {
+
+            try {
+                String s = MainActivity.comman.startSiddhiApp(appList.get(position));
+                return s != null;
+            } catch (RemoteException e) {
+                Toast.makeText(null, "Error in executing siddhi app",
+                        Toast.LENGTH_SHORT).show();
+            }
+            return false;
+
+        }
+
+        private boolean stopApp(int position) {
+
+            try {
+                MainActivity.comman.stopSiddhiApp(appList.get(position));
+                return true;
+            } catch (RemoteException e) {
+                Toast.makeText(null, "Error in executing siddhi app",
+                        Toast.LENGTH_SHORT).show();
+            }
+            return false;
+
+        }
+
+        @Override
+        public int getCount() {
+            return appList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+    }
 }
+
--- sample-siddhi-app/src/main/java/org/wso2/siddhi/android/sample/SensorAppActivity.java
+++ sample-siddhi-app/src/main/java/org/wso2/siddhi/android/sample/SensorAppActivity.java
@@ -1,13 +1,270 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.siddhi.android.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+import org.wso2.siddhi.android.platform.SiddhiAppController;
+import org.wso2.siddhi.android.platform.SiddhiAppService;
+import org.wso2.siddhi.android.platform.util.SiddhiAndroidException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SensorAppActivity extends AppCompatActivity {
+
+    private DataUpdateReceiver dataUpdateReceiver;
+
+    private String inSteams[] = {
+            "@source(type='android-accelerometer', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='accelerationX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-game-rotation', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='rotationX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-gravity', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='gravityX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+
+            "@source(type='android-gyroscope', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='rotationX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-humidity', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='humidity')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-light', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='light')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+
+            "@source(type='android-linear-accelerometer', @map(type='keyvalue'," +
+                    "fail.on.missing.attribute='false'," +
+                    "@attributes(sensor='sensor',vector='accelerationX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-magnetic', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='magneticX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-pressure', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(sensor='sensor',vector='pressure')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+
+            "@source(type='android-proximity', @map(type='keyvalue',fail.on.missing.attribute=" +
+                    "'false',@attributes(sensor='sensor',vector='proximity')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-rotation', @map(type='keyvalue',fail.on.missing.attribute=" +
+                    "'false',@attributes(sensor='sensor',vector='rotationX')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-temperature', @map(type='keyvalue',fail.on.missing.attribute=" +
+                    "'false',@attributes(sensor='sensor',vector='temperature')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-steps', @map(type='keyvalue',fail.on.missing.attribute=" +
+                    "'false',@attributes(sensor='sensor',vector='steps')))" +
+                    "define stream sensorInStream ( sensor string, vector float);",
+            "@source(type='android-location', @map(type='keyvalue',fail.on.missing.attribute" +
+                    "='false',@attributes(lo='longitude',la='latitude')))" +
+                    "define stream sensorInStream ( lo double, la double);",
+
+    };
+    private String inNames[] = {"Accelerometer", "GameRotation", "Gravity",
+            "gyroscope", "humidity", "light",
+            "linear accelerometer", "magnetic", "pressure",
+            "proximity",
+            "rotation", "temperature", "steps", "location"};
+
+    private String broadcastIdentifier = "sample.siddhi.app";
+    private String outStream[] = {
+            "@sink(type='android-broadcast' , identifier='" + broadcastIdentifier + "', " +
+                    "@map(type='keyvalue'))" +
+                    "define stream outputStream (sensor string, vector float); ",
+            "@sink(type='android-notification' , title='Details'," +
+                    "multiple.notifications = 'true', @map(type='keyvalue'))" +
+                    "define stream outputStream (sensor string, vector float); ",
+            "@sink(type='android-sound',time = '5' , @map(type='keyvalue'))" +
+                    "define stream outputStream (sensor string, vector float); "
+    };
+    private String outputStreamNames[] = {
+            "Broadcast", "Notification", "Ringing",
+    };
+    private String startLine = "@app:name('bar')";
+    private String endLine = "from sensorInStream select * insert into outputStream";
+    private final int MY_PERMISSION_ACCESS_LOCATION = 0;
+    private String runningAppName = "";
+
+    private ListView listView;
+    private List<String> messageList = new ArrayList<>();
+    private ArrayAdapter<String> listAdapter;
+    private Spinner spinner1;
+    private Spinner spinner2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_app);
+        listView = findViewById(R.id.messageList);
+        spinner1 = findViewById(R.id.spinner);
+        spinner2 = findViewById(R.id.spinner2);
+
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                messageList);
+        listView.setAdapter(listAdapter);
+
+        spinner1.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, inNames));
+        spinner2.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, outputStreamNames));
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                Toast.makeText(this,
+                        "Location permission given", Toast.LENGTH_SHORT).show();
+
+            } else {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSION_ACCESS_LOCATION);
+
+                Toast.makeText(this,
+                        "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_ACCESS_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.e("Location", "Permission given");
+
+                } else {
+                    Log.e("Location", "Permission not given");
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (dataUpdateReceiver == null) {
+            dataUpdateReceiver = new DataUpdateReceiver();
+        }
+        IntentFilter intentFilter = new IntentFilter(broadcastIdentifier);
+        registerReceiver(dataUpdateReceiver, intentFilter);
+    }
+
+    /**
+     * Bind the Android app to Sidddhi Service
+     *
+     * @param view
+     */
+    public void bindToSiddhiService(View view) {
+
+
+    }
+
+    /**
+     * Send the app stream to SiddhiService
+     *
+     * @param view
+     */
+    public void sendApp(View view) { //change name
+
+        if (!runningAppName.equalsIgnoreCase("")) {
+            stopApp(null);
+        }
+        try {
+            int inIndex = spinner1.getSelectedItemPosition();
+            int outIndex = spinner2.getSelectedItemPosition();
+
+            String app = startLine + inSteams[inIndex] + outStream[outIndex] + endLine;
+            Log.e("App",app);
+            String appName = MainActivity.comman.startSiddhiApp(app);
+            this.runningAppName = appName;
+            if (appName != null) {
+                Toast.makeText(this, "Send the query : ", Toast.LENGTH_LONG).show();
+
+            }
+        } catch (RemoteException e) {
+            Log.e("SampleApp", Log.getStackTraceString(e));
+        } catch (SiddhiAndroidException e) {
+            Toast.makeText(this, "Error in creating siddhi app",
+                    Toast.LENGTH_SHORT).show();
+            Log.e("Error", Log.getStackTraceString(e));
+        }
+    }
+
+    /**
+     * stop a running siddhi app
+     *
+     * @param view
+     */
+    public void stopApp(View view) {
+        try {
+            MainActivity.comman.stopSiddhiApp(runningAppName);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Broadcast receiver to get intents from the Siddhi Service
+     * Has a hardcoded intent filter to match the query
+     */
+    private class DataUpdateReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(broadcastIdentifier)) {
+                messageList.add(intent.getStringExtra("sensor") + " : " +
+                        String.valueOf(intent.getFloatExtra("vector", 0)));
+//                messageList.add(intent.getDoubleExtra("lo",0)+" : "+String.valueOf(intent.getDoubleExtra("la",0)));
+                listAdapter.notifyDataSetChanged();
+            }
+        }
     }
 }
